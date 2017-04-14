162c3397cb8b3210f379f963f4a3219aa43ce595
Zhou Yu <yuzhou03@qq.com>
2017-04-14 18:14:56 +0800

    use example config file

--- util/zplayutil.py
+++ util/zplayutil.py
@@ -6,7 +6,7 @@ import os
 scriptdir = os.path.dirname(__file__)
 
 
-def get_config_dict(section='mysql', cfg_file=os.path.join(scriptdir, '../conf/app.conf')):
+def get_config_dict(section='mysql', cfg_file=os.path.join(scriptdir, '../conf/app.conf.example')):
     config = ConfigParser.RawConfigParser()
     config.read(cfg_file)
     return dict(config.items(section))
